454b871bdaf70011a3439bf23cee9bd36d29aa53
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-03-10 21:07:10 +0800

    [vis/VisNetworkDemos] Integrated with dynamical JavaScript loader.

--- vis/VisNetworkDemos.tsx
+++ vis/VisNetworkDemos.tsx
@@ -1,6 +1,7 @@
 //
 
 import React from 'react';
+import {loadScript} from 'src/utils/HtmlScriptLoader';
 import {R} from './resources';
 import {useStyles} from './styles';
 import {drawMonitoringDiagramWithVis} from './vis-drawer';
@@ -13,11 +14,24 @@ export const VisNetworkDemos = React.memo(({}: IProps) => {
 	const cls = useStyles();
 	const domDiagramRootRef = React.useRef(null);
 
+	const [cached, setCached] = React.useState(false);
+
 	React.useLayoutEffect(() => {
 		if (!domDiagramRootRef.current) {return;}
 		console.log('Drawing into domDiagramRoot:', domDiagramRootRef.current);
+		// https://unpkg.com/vis-network/standalone/umd/vis-network.min.js
+		if (!window['vis']) {
+			console.warn('Loading the depended script now!', new Date().toLocaleString());
+			loadScript('https://unpkg.com/vis-network/standalone/umd/vis-network.min.js').then(() => {
+				console.log('Successfully loaded the target script.', new Date().toLocaleString());
+				setCached(true);
+			}).catch((ex) => {
+				console.error('Failed to load the target script:', ex, new Date().toLocaleString());
+			});
+			return;
+		}
 		drawMonitoringDiagramWithVis({}, domDiagramRootRef.current);
-	}, []);
+	}, [cached]);
 
 	return (
 		<div className={cls.root}>
--- vis/vis-drawer.ts
+++ vis/vis-drawer.ts
@@ -3,10 +3,9 @@
 import {IOptions} from './drawer-options';
 import {mockVisData} from './vis-mock-nodes';
 
-const vis = window['vis'];
-
 // Called when the Visualization API is loaded.
 export const drawMonitoringDiagramWithVis = (options: IOptions = {}, dom: any): [any] => {
+	const vis = window['vis'];
 	const network = new vis.Network(dom, mockVisData, options);
 	return [network];
 };
